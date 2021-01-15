8dcf002e52f0ba454655b9df99666e831a85a1e2
Souad <souadtouat45@gmail.com>
2021-01-15 12:14:46 +0100

    déclaration des variable globale

---
+++ jeuPondu/script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+my_word="pomme"
+
+error_cont=0
+
+
+print (my_word)
+
+while error_cont <= 5:
+
+	my_letter = input("entrer une lettre")
+
+    print ( my_letter)
+    
